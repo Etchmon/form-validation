531b96036ada9f7b54718685fb869a9465a77d05
James Etchells <etch.rush@gmail.com>
2022-03-21 15:19:55 -0700

    Add script to generate html form

--- app.js
+++ app.js
@@ -18,10 +18,50 @@ const Display = (() => {
             required: 'required',
         },
         {
-            type: 'email',
-            name: 'email',
-            placeholder: 'Email Address',
+            type: 'text',
+            name: 'zipCode',
+            placeholder: 'Zip Code',
+            pattern: "[0-9]{5}",
+            required: 'required',
+        },
+        {
+            type: 'password',
+            name: 'password',
+            placeholder: 'Password',
+            required: 'required',
+        },
+        {
+            type: 'password',
+            name: 'confirmPassword',
+            placeholder: 'Password',
             required: 'required',
         }
     ];
+
+    inputs.forEach(data => {
+        const container = document.createElement('div');
+        const label = document.createElement('label');
+        const input = document.createElement('input');
+
+        label.innerHTML = data.placeholder;
+
+        Object.assign(input, data);
+
+        container.appendChild(label);
+        container.appendChild(input);
+
+        form.appendChild(container);
+    });
+
+    const initiateDom = (() => {
+        Object.assign(btn, {
+            type: 'Submit',
+            innerHTML: 'Submit'
+        })
+
+        container.appendChild(form);
+        container.appendChild(btn);
+        dom.appendChild(container);
+    })();
+
 })();
